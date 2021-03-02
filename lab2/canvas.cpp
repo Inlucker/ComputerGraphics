e2ac1b2a6b6ef8d529317b73efe05ca98a45ac55
Arseny Pronin <arseny707@mail.ru>
2021-03-02 20:13:47 +0300

    solved povorot formula

--- lab2/canvas.cpp
+++ lab2/canvas.cpp
@@ -56,25 +56,17 @@ void Canvas::paintEvent(QPaintEvent *event)
     //painter.drawPoint(0, 0);
     //painter.rotate(angle);
 
+    //-----------------------SET PARAMS START-----------------------
     //Set radians by angle
     radians = angle * PI / 180;
-
     //Set radius by size
-    //float rad = size * 25;
     float rad_x = size_x * 25;
     float rad_y = size_y * 25;
-
     //Set head params
     float head_center_x = x;
     float head_center_y = y - rad_y * 2;
-    rotate(&head_center_x, &head_center_y);
-    QPointF head_center = QPointF(head_center_x, head_center_y);
-    //int head_size = rad;
     int head_size_x = rad_x;
     int head_size_y = rad_y;
-    //Draw head
-    painter.drawEllipse(head_center, head_size_x, head_size_y);
-
     //Set ears params
     float ear_offset = size_x * 5;
     float ear_height = size_y * 15;
@@ -114,14 +106,48 @@ void Canvas::paintEvent(QPaintEvent *event)
     float y23 = head_center_y - delta_y2;
     ear2_dots[2].setX(x23);
     ear2_dots[2].setY(y23);
+    //Set eyes params
+    float eye_width = size_x * 10;
+    float eye_height = size_y * 5;
+    int eye_size_x = size_x * 5;
+    int eye_size_y = size_y * 5;
+    float eye1_x = head_center_x - eye_width;
+    float eye1_y = head_center_y - eye_height;
+    float eye2_x = head_center_x + eye_width;
+    float eye2_y = head_center_y - eye_height;
+    //Set whiskers params
+    float len = size_x * 35;
+    float wisk_height = size_y * 10;
+    float wisk_center_x = x;
+    float wisk_center_y = y - rad_y * 2 + wisk_height;
 
-    rotate(&x11, &y11);
-    rotate(&x21, &y21);
-    rotate(&x12, &y12);
-    rotate(&x22, &y22);
-    rotate(&x13, &y13);
-    rotate(&x23, &y23);
+    float wisk_x1 = wisk_center_x - len;
+    float wisk_x2 = wisk_center_x + len;
+    float wisk_y1 = wisk_center_y;
+    float wisk_y2 = wisk_center_y + wisk_height;
+    float wisk_y3 = wisk_center_y - wisk_height;
 
+    QPointF wiskers_dots[7];
+    wiskers_dots[0].setX(wisk_center_x); wiskers_dots[0].setY(wisk_center_y);
+    wiskers_dots[1].setX(wisk_x1); wiskers_dots[1].setY(wisk_y1);
+    wiskers_dots[2].setX(wisk_x1); wiskers_dots[2].setY(wisk_y2);
+    wiskers_dots[3].setX(wisk_x1); wiskers_dots[3].setY(wisk_y3);
+    wiskers_dots[4].setX(wisk_x2); wiskers_dots[4].setY(wisk_y1);
+    wiskers_dots[5].setX(wisk_x2); wiskers_dots[5].setY(wisk_y2);
+    wiskers_dots[6].setX(wisk_x2); wiskers_dots[6].setY(wisk_y3);
+    //Set body params
+    float body_center_x = x;
+    float body_center_y = y + rad_y;
+    int body_width = rad_x;
+    int body_height = rad_y * 2;
+    QPointF body_center = QPointF(body_center_x, body_center_y);
+    //------------------------SET PARAMS END------------------------
+
+    //-------------------------ROTATE START-------------------------
+    //Rotate head
+    rotate(&head_center_x, &head_center_y);
+    QPointF head_center = QPointF(head_center_x, head_center_y);
+    //Rotate ears
     for (int i = 0; i < 3; i++)
     {
         float tmp_x = ear1_dots[i].x();
@@ -136,49 +162,12 @@ void Canvas::paintEvent(QPaintEvent *event)
         ear2_dots[i].setX(tmp_x);
         ear2_dots[i].setY(tmp_y);
     }
-    //Draw ears
-    for (int i = 0; i < 2; i++)
-    {
-        painter.drawLine(ear1_dots[i], ear1_dots[i + 1]);
-        painter.drawLine(ear2_dots[i], ear2_dots[i + 1]);
-    }
-
-    //Set eyes params
-    float eye_width = size_x * 10;
-    float eye_height = size_y * 5;
-    //int eye_size = size * 5;
-    int eye_size_x = size_x * 5;
-    int eye_size_y = size_y * 5;
-    float eye1_x = head_center_x - eye_width;
-    float eye1_y = head_center_y - eye_height;
-    float eye2_x = head_center_x + eye_width;
-    float eye2_y = head_center_y - eye_height;
+    //Rotate eyes
     rotate(&eye1_x, &eye1_y);
+    rotate(&eye2_x, &eye2_y);
     QPointF eye_center1 = QPointF(eye1_x, eye1_y);
     QPointF eye_center2 = QPointF(eye2_x, eye2_y);
-    //Draw eyes
-    painter.drawEllipse(eye_center1, eye_size_x, eye_size_y);
-    painter.drawEllipse(eye_center2, eye_size_x, eye_size_y);
-
-    //Set whiskers params DONE
-    float len = size_x * 35;
-    float wisk_height = size_y * 10;
-    float wisk_center_x = x;
-    float wisk_center_y = y - rad_y * 2 + wisk_height;
-
-    float wisk_x1 = wisk_center_x - len;
-    float wisk_x2 = wisk_center_x + len;
-    float wisk_y1 = wisk_center_y;
-    float wisk_y2 = wisk_center_y + wisk_height;
-    float wisk_y3 = wisk_center_y - wisk_height;
-    QPointF wiskers_dots[7];
-    wiskers_dots[0].setX(wisk_center_x); wiskers_dots[0].setY(wisk_center_y);
-    wiskers_dots[1].setX(wisk_x1); wiskers_dots[1].setY(wisk_y1);
-    wiskers_dots[2].setX(wisk_x1); wiskers_dots[2].setY(wisk_y2);
-    wiskers_dots[3].setX(wisk_x1); wiskers_dots[3].setY(wisk_y3);
-    wiskers_dots[4].setX(wisk_x2); wiskers_dots[4].setY(wisk_y1);
-    wiskers_dots[5].setX(wisk_x2); wiskers_dots[5].setY(wisk_y2);
-    wiskers_dots[6].setX(wisk_x2); wiskers_dots[6].setY(wisk_y3);
+    //Rotate whiskers
     for (int i = 0; i < 7; i++)
     {
         float tmp_x = wiskers_dots[i].x();
@@ -187,26 +176,33 @@ void Canvas::paintEvent(QPaintEvent *event)
         wiskers_dots[i].setX(tmp_x);
         wiskers_dots[i].setY(tmp_y);
     }
-    //QPointF wisk_center = QPointF(wisk_center_x, wisk_center_y);
-    //Draw cat's whiskers DONE
+    //--------------------------ROTATE END--------------------------
+
+    //--------------------------DRAW START--------------------------
+    //Draw head
+    painter.drawEllipse(head_center, head_size_x, head_size_y);
+    //Draw ears
+    for (int i = 0; i < 2; i++)
+    {
+        painter.drawLine(ear1_dots[i], ear1_dots[i + 1]);
+        painter.drawLine(ear2_dots[i], ear2_dots[i + 1]);
+    }
+    //Draw eyes
+    painter.drawEllipse(eye_center1, eye_size_x, eye_size_y);
+    painter.drawEllipse(eye_center2, eye_size_x, eye_size_y);
+    //Draw cat's whiskers
     for (int i = 1; i < 7; i++)
     {
         painter.drawLine(wiskers_dots[0], wiskers_dots[i]);
     }
-
-    //Set body params
-    float body_center_x = x;
-    float body_center_y = y + rad_y;
-    int body_width = rad_x;
-    int body_height = rad_y * 2;
-    QPointF body_center = QPointF(body_center_x, body_center_y);
     //Draw body
     painter.drawEllipse(body_center, body_width, body_height);
     //painter.drawEllipse(QPointF(x, y), rad, rad);
     //painter.drawEllipse(QPointF(x, y + rad * 2), rad, rad);
+    //---------------------------DRAW END---------------------------
 
 
-    pair<float, float> square[4];
+    /*pair<float, float> square[4];
     float a = -100;
     float b = 100;
     //float c = (b + a) / 2;
@@ -214,40 +210,17 @@ void Canvas::paintEvent(QPaintEvent *event)
     square[1] = make_pair(float(b), float(a));
     square[2] = make_pair(float(b), float(b));
     square[3] = make_pair(float(a), float(b));
-    /*for (int i = 0; i < 4; i++)
-    {
-        cout << i << ": X = " << square[i].first << "; Y = " << square[i].second << ";" << endl;
-    }
-    cout << endl;*/
 
-    //cout << endl;
     for (int i = 0; i < 4; i++)
     {
-        //cout << i << ": X = " << square[i].first << " -> ";
-        //cout << i << ": X = " << square[i].first << "; Y = " << square[i].second << endl;
-        /*float new_x = rotate_x(square[i].first, square[i].second, c, c, angle * PI / 180);
-        float new_y = rotate_y(square[i].first, square[i].second, c, c, angle * PI / 180);
-        square[i].first = new_x;
-        //cout << square[i].first << endl;
-        //cout << i << ": Y = " << square[i].second << " -> ";
-        square[i].second = new_y;*/
-        //cout << square[i].second << endl;
-
         rotate(&square[i].first, &square[i].second);
     }
-    //cout << endl;
-
-    /*for (int i = 0; i < 4; i++)
-    {
-        cout << i << ": X = " << square[i].first << "; Y = " << square[i].second << ";" << endl;
-    }
-    cout << endl;*/
 
     for (int i = 0; i < 3; i++)
     {
         painter.drawLine(square[i].first, square[i].second, square[i + 1].first, square[i + 1].second);
     }
-    painter.drawLine(square[3].first, square[3].second, square[0].first, square[0].second);
+    painter.drawLine(square[3].first, square[3].second, square[0].first, square[0].second);*/
 
     painter.end(); //Освобождение контекста
 }
